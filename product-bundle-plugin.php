44a3f95768e44f31c55c9fbd4175834edf2ca8de
sttech321 <108738642+sttech321@users.noreply.github.com>
2024-05-08 19:03:02 +0530

    Add files via upload

---
+++ product-bundle-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Product Bundle
+Version: 0.1
+Description: Offer personalized product bundles, bulk discount packages, and assembled products.
+Author: Supremetechnologies
+Author URI: https://supremetechnologiesindia.com/
+Text-domain: product-bundle
+*/
+
+// include admin.php file code  for attach admin menu of product bundle.
+
+include(plugin_dir_path(__FILE__) . "/include/functions.php");
+include(plugin_dir_path(__FILE__) . '/admin/admin.php');
+
+// $plugin_dir_path = plugin_dir_path(__FILE__);
+// include($plugin_dir_path . '/admin/admin.php');
+
+?>
